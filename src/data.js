d49f16d3b761502178330eed653862e2fd6b24c3
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-07-26 12:36:59 +0200

    Add more products to store data

--- src/data.js
+++ src/data.js
@@ -3,7 +3,17 @@ const products = [
   {name: "Generic Fresh Mouse", price: 493.00},
   {name: "Incredible Wooden Fish", price: 595.00},
   {name: "Generic Metal Shoes", price: 648.00},
-  {name: "Rustic Cotton Mouse", price: 370.00}
+  {name: "Rustic Cotton Mouse", price: 370.00},
+  {name: "Tasty Rubber Mouse", price: 352.00},
+  {name: "Refined Soft Towels", price: 125.00},
+  {name: "Refined Cotton Fish", price: 164.00},
+  {name: "Licensed Fresh Hat", price: 428.00},
+  {name: "Sleek Steel Pants", price: 581.00},
+  {name: "Awesome Frozen Computer", price: 914.00},
+  {name: "Rustic Steel Shoes", price: 852.00},
+  {name: "Intelligent Cotton Chair", price: 20.00},
+  {name: "Rustic Fresh Car", price: 559.00},
+  {name: "Gorgeous Wooden Car", price: 129.00}
 ];
 
 export default products;
